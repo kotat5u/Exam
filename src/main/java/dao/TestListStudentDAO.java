02666d0dba5cb9f081296f09159a3d8cd80bbc92
k kato <k kato@192.168.11.5>
2024-04-17 11:13:02 +0900

    成績参照完成

---
+++ src/main/java/dao/TestListStudentDAO.java
@@ -0,0 +1,44 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.Student;
+import bean.TestListStudent;
+
+public class TestListStudentDAO extends DAO{
+	
+	private String baseSql="select * from test inner join subject on test.subject_cd = subject.cd";
+	
+	public List<TestListStudent> postfilter(ResultSet rSet) throws Exception {
+		List<TestListStudent> list = new ArrayList<>();
+		while (rSet.next()) {
+			TestListStudent t=new TestListStudent();
+			t.setSubjectName(rSet.getString("name"));
+			t.setSubjectCd(rSet.getString("subject.cd"));
+			t.setNum(rSet.getInt("test.no"));
+			t.setPoint(rSet.getInt("point"));
+			list.add(t);
+		}
+		return list;
+	}
+	
+	public List<TestListStudent> filter(Student student) throws Exception {
+		List<TestListStudent> list = new ArrayList<>();
+		Connection con = getConnection();
+//		
+		PreparedStatement st=con.prepareStatement(baseSql + " where student_no = ?");
+		st.setString(1, student.getNo());
+		ResultSet rs=st.executeQuery();
+//		SQLの結果をListに格納する処理
+		list = postfilter(rs);
+		
+		st.close();
+		con.close();
+
+		return list;
+	}
+}
